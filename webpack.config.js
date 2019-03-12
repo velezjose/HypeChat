e644be3bb1311391476be8142cbc2d410d698916
Jose Velez <josevelezq7@gmail.com>
2019-03-12 14:55:46 -0700

    Configure webpack and add it as dependency

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
